a8256c250a2adc83862a4481c54c08f8c66e3739
YongboYang <yangyongbo716@outlook.com>
2026-04-05 11:23:06 -0700

    Update model.py

--- qwen/model.py
+++ qwen/model.py
@@ -192,7 +192,7 @@ class Qwen2_5_VLModelWithTree(Qwen2_5_VLModel):
             A_generic = get_attn_scores(generic_embeds, generic_attn_mask, generic_pos_ids)
 
             # relative attention: element-wise division
-            patch_scores_global = A_q / (A_generic + 1e-8)
+            patch_scores_global = A_q
             s_min = patch_scores_global.min()
             s_max = patch_scores_global.max()
             patch_scores_global = (patch_scores_global - s_min) / (s_max - s_min + 1e-6)
