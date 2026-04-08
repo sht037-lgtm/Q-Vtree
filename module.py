c44521866f26eed20c13a64e6f40d819c5656faa
YongboYang <yangyongbo716@outlook.com>
2026-04-07 23:00:36 -0700

    Update module.py

--- module.py
+++ module.py
@@ -441,8 +441,8 @@ class QVTree(nn.Module):
         D: int,
         Dq: Optional[int] = None,
         use_proj_if_needed: bool = True,
-        split_threshold: float = 0.3,
-        softmax_temperature: float = 1.0,
+        split_threshold: float = 0.2,
+        softmax_temperature: float = 0.5,
         eps: float = 1e-6,
     ):
         super().__init__()
